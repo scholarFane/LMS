03ccc06bc288b1284e4872be316a627a0c4aea8a
Cactus635 <68587772+scholarFane@users.noreply.github.com>
2021-10-28 09:23:30 +0700

    init
    everything so far

---
+++ README.md
@@ -0,0 +1,23 @@
+# Laravel 8 Learning Management System (LMS) admin 
+
+[![N|Solid](https://cldup.com/dTxpPi9lDf.thumb.png)](https://nodesource.com/products/nsolid)
+
+[![Build Status](https://travis-ci.org/joemccann/dillinger.svg?branch=master)](https://travis-ci.org/joemccann/dillinger)
+
+Dillinger is a cloud-enabled, mobile-ready, offline-storage compatible,
+AngularJS-powered HTML5 Markdown editor.
+
+- Create database and adjust setting in .env file
+- Run command: composer update
+- Migrate database with seed: php artisan migrate --seed
+- Run php artisan serve 
+ 
+**Login Detail**
+Username: admin@admin.com
+Password: password
+
+## License
+
+MIT
+
+**Free Software, Hell Yeah!**
---
+++ routes/web.php
@@ -0,0 +1,76 @@
+<?php
+
+Route::redirect('/', '/login');
+Route::get('/home', function () {
+    if (session('status')) {
+        return redirect()->route('admin.home')->with('status', session('status'));
+    }
+
+    return redirect()->route('admin.home');
+});
+
+Route::get('userVerification/{token}', 'UserVerificationController@approve')->name('userVerification');
+Auth::routes();
+
+Route::group(['prefix' => 'admin', 'as' => 'admin.', 'namespace' => 'Admin', 'middleware' => ['auth']], function () {
+    Route::get('/', 'HomeController@index')->name('home');
+    // Permissions
+    Route::delete('permissions/destroy', 'PermissionsController@massDestroy')->name('permissions.massDestroy');
+    Route::resource('permissions', 'PermissionsController');
+
+    // Roles
+    Route::delete('roles/destroy', 'RolesController@massDestroy')->name('roles.massDestroy');
+    Route::resource('roles', 'RolesController');
+
+    // Users
+    Route::delete('users/destroy', 'UsersController@massDestroy')->name('users.massDestroy');
+    Route::resource('users', 'UsersController');
+
+    // Courses
+    Route::delete('courses/destroy', 'CoursesController@massDestroy')->name('courses.massDestroy');
+    Route::post('courses/media', 'CoursesController@storeMedia')->name('courses.storeMedia');
+    Route::post('courses/ckmedia', 'CoursesController@storeCKEditorImages')->name('courses.storeCKEditorImages');
+    Route::resource('courses', 'CoursesController');
+
+    // Lessons
+    Route::delete('lessons/destroy', 'LessonsController@massDestroy')->name('lessons.massDestroy');
+    Route::post('lessons/media', 'LessonsController@storeMedia')->name('lessons.storeMedia');
+    Route::post('lessons/ckmedia', 'LessonsController@storeCKEditorImages')->name('lessons.storeCKEditorImages');
+    Route::resource('lessons', 'LessonsController');
+
+    // Tests
+    Route::delete('tests/destroy', 'TestsController@massDestroy')->name('tests.massDestroy');
+    Route::resource('tests', 'TestsController');
+
+    // Questions
+    Route::delete('questions/destroy', 'QuestionsController@massDestroy')->name('questions.massDestroy');
+    Route::post('questions/media', 'QuestionsController@storeMedia')->name('questions.storeMedia');
+    Route::post('questions/ckmedia', 'QuestionsController@storeCKEditorImages')->name('questions.storeCKEditorImages');
+    Route::resource('questions', 'QuestionsController');
+
+    // Question Options
+    Route::delete('question-options/destroy', 'QuestionOptionsController@massDestroy')->name('question-options.massDestroy');
+    Route::resource('question-options', 'QuestionOptionsController');
+
+    // Test Results
+    Route::delete('test-results/destroy', 'TestResultsController@massDestroy')->name('test-results.massDestroy');
+    Route::resource('test-results', 'TestResultsController');
+
+    // Test Answers
+    Route::delete('test-answers/destroy', 'TestAnswersController@massDestroy')->name('test-answers.massDestroy');
+    Route::resource('test-answers', 'TestAnswersController');
+
+    // User Alerts
+    Route::delete('user-alerts/destroy', 'UserAlertsController@massDestroy')->name('user-alerts.massDestroy');
+    Route::get('user-alerts/read', 'UserAlertsController@read');
+    Route::resource('user-alerts', 'UserAlertsController', ['except' => ['edit', 'update']]);
+});
+Route::group(['prefix' => 'profile', 'as' => 'profile.', 'namespace' => 'Auth', 'middleware' => ['auth']], function () {
+    // Change password
+    if (file_exists(app_path('Http/Controllers/Auth/ChangePasswordController.php'))) {
+        Route::get('password', 'ChangePasswordController@edit')->name('password.edit');
+        Route::post('password', 'ChangePasswordController@update')->name('password.update');
+        Route::post('profile', 'ChangePasswordController@updateProfile')->name('password.updateProfile');
+        Route::post('profile/destroy', 'ChangePasswordController@destroy')->name('password.destroyProfile');
+    }
+});
